9ced29af2c200b11d7350a1d780f31b37e2e3efc
abarna10303 <abarnas361@gmail.com>
2022-12-13 17:19:29 +0530

    Area of Triangel

---
+++ main.js
@@ -0,0 +1,6 @@
+/*AREA OF TRIANGLE PROGRAM*/
+var height,birth;
+height=parseInt(prompt("Enter the height"));
+birth=parseInt(prompt("Enter the Birht"));
+t_area=height*birth/2;
+console.log("Area of Triangel is : " +t_area);
